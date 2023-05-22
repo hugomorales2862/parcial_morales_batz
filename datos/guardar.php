a727c3e425e86e6aa88b2536e22d23b0e64f9394
Hugo Morales <moralesbatzh@gmail.com>
2023-05-22 08:34:53 -0600

    se coloca la estructura del try, catch

--- datos/guardar.php
+++ datos/guardar.php
@@ -1,5 +1,14 @@
 <?php
 require '../parcial_morales_batz/conexion/cursos.php';
 
+try {
+    $curso = new Curso ($_POST);
+    $resultado = $producto-> guardar();
+    $error = "NO SE GUARDARON LOS DATOS";
 
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+}catch (Exception $e2){
+    $error = $e2->getMessage();
+}
 ?>
