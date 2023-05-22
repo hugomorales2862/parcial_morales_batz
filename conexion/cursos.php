7464a4c7364a4bd5ffd3575bdc810715c9215782
Hugo Morales <moralesbatzh@gmail.com>
2023-05-22 08:13:54 -0600

    inicio a crear el metodo constructor de la classe curso

--- conexion/cursos.php
+++ conexion/cursos.php
@@ -3,7 +3,16 @@ require 'conexion.php';
 
 class Curso extends Conexion {
     public $id;
-    p
+    public $nombre;
+    public $periodos;
+    public $fase;
+    public $instructor;
+    public $creditos;
+    public function __construct ($args = [])
+    {
+        $this->=$args []
+    }
+
 }
 
 ?>
