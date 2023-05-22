184b7ec388275e801b4d2a0ba5b4478d6dc67a7d
Hugo Morales <moralesbatzh@gmail.com>
2023-05-22 09:03:44 -0600

    cambiamos la ruta de la base de datos pues ahora se llama curso

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -5,7 +5,7 @@
 
         private static function conectar(){
             try {
-                self::$conexion = new PDO ('informix:host=host.docker.internal; service=9088; database=mdn; server=informix; protocol=onsoctcp;EnableScrollableCursors = 1','informix','in4mix');
+                self::$conexion = new PDO ('informix:host=host.docker.internal; service=9088; database=curso; server=informix; protocol=onsoctcp;EnableScrollableCursors = 1','informix','in4mix');
                 ECHO "CONECTADO";
             } catch (PDOException $e) {
                 echo "Error de conexion a la base de datos";
