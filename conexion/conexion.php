b7e40478335587a8810a505aba65029074471806
Hugo Morales <moralesbatzh@gmail.com>
2023-05-22 08:06:02 -0600

    terminamos de crear nustra funcion ejecutar

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,23 +1,28 @@
 <?php
 
-abstract class Conexion {
-    public static $conexion = null;
+    abstract class Conexion {
+        public static $conexion = null;
 
-    private static function conectar(){
-        try {
-            self::$conexion = new PDO ('informix:host=host.docker.internal; service=9088; database=mdn; server=informix; protocol=onsoctcp;EnableScrollableCursors = 1','informix','in4mix');
-            ECHO "CONECTADO";
-        } catch (PDOException $e) {
-            echo "Error de conexion a la base de datos";
-            echo "<br>";
-            echo $e->getMessage();
-            exit;
+        private static function conectar(){
+            try {
+                self::$conexion = new PDO ('informix:host=host.docker.internal; service=9088; database=mdn; server=informix; protocol=onsoctcp;EnableScrollableCursors = 1','informix','in4mix');
+                ECHO "CONECTADO";
+            } catch (PDOException $e) {
+                echo "Error de conexion a la base de datos";
+                echo "<br>";
+                echo $e->getMessage();
+                exit;
+            }
+            return self::$conexion;
         }
-        return self::$conexion;
-    }
 
-public static function ejecutar ($sql){
-    s
-}
+        public static function ejecutar($sql){
+        self::conectar();
+        $sentencia = self::$conexion->prepare($sql);
+        $resultado = $sentencia->execute();
+        self::$conexion = null;
+         return $resultado;    
+    
+     }
 
 }
