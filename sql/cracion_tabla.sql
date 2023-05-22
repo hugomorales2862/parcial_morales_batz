e65a6da1b0461ea664af2f4b6a37833aa03538e6
Hugo Morales <moralesbatzh@gmail.com>
2023-05-22 09:02:24 -0600

    finalizmos de crear nuestra tabla, para almacenar los datos obtenidos

--- sql/cracion_tabla.sql
+++ sql/cracion_tabla.sql
@@ -2,4 +2,13 @@
 
 create database curso 
 
+-- creamos nuestra tabla con el nombre curso
+create table curso(
+    id int not null,
+    nombre varchar (50),
+    periodos int,
+    fase varchar (50),
+    instructor varchar (50),
+    creditos varchar (50)
 
+)
